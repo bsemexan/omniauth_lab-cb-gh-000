62582ab1e214dfff60c139ece6b13a757536e57a
Berry  Semexan <bsemexan@gmail.com>
2018-04-12 17:38:55 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20180412173202) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.integer  "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
