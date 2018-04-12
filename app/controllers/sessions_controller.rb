c0da0e05af96bf6ecd13553d7e9cf75eacc9f8d9
Berry  Semexan <bsemexan@gmail.com>
2018-04-12 17:56:40 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_by(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
     end
+    session[:user_id] = @user.id
   end
 
   def auth
